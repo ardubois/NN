e8887ebb8d6750f23270efb1ea04a4a48c3359bf
ardubois <rauberdubois@gmail.com>
2023-01-02 12:23:57 -0300

    better work creating for bacht gradient

--- matrex.exs
+++ matrex.exs
@@ -25,7 +25,8 @@ defmodule NN do
   def runNet(input,[weights_l],target,lr) do
     #IO.inspect(weights_l)
     #raise "hell"
-    o = dotPSize(1000,input,weights_l)
+    #o = dotPSize(5,input,weights_l)
+    o = Matrex.dot(input,weights_l)
     finalDerivative = Matrex.subtract(target,o)
     error =  Matrex.sum(Matrex.apply(finalDerivative,fn(n)-> n**2 end))
     correct = if (Matrex.argmax(o)==Matrex.argmax(target)) do 1 else 0 end
@@ -34,7 +35,10 @@ defmodule NN do
     {[newWeights],nextLayerD,error,correct}
   end
   def runNet(input,[w|tl],target,lr) do
-    o =  relu(Matrex.dot(input,w))
+    #o =  relu(Matrex.dot(input,w))
+    #IO.inspect(w)
+    #raise "hell"
+    o =  relu(dotPSize(1440,input,w))
     #IO.inspect o
     #raise "oi"
     {net,wD,error,correct} = runNet(o,tl,target,lr)
@@ -191,6 +195,42 @@ defmodule NN do
     cols =getColumns(restMatrix,ncol-1)
     [col|cols]
   end
+  def run_batch(size,input,weights,target,lr) do
+    tasks = slice_entries(size,input,target,weights,lr)
+    #tasks = Enum.map(list, fn({i1,t1}) -> Task.async(fn -> NN.runNet(i1,weights,t1,lr)end) end)
+    #results = Enum.map(tasks,&Task.await/1)
+    [hr|tr] = tasks
+    r1 = Task.await(hr)
+    List.foldr(tr, r1, fn(task,{nn2,wd2,erro2,acc2}) -> {nn1,wd1,erro1,acc1} = Task.await task
+                                                        {sumNNs(nn1,nn2),wd1,erro1+erro2,acc1+acc2} end)
+  end
+  def sumNNs([w1],[w2]) do
+    w3 =Matrex.add(w1,w2)
+    [w3]
+  end
+  def sumNNs([w1|t1],[w2|t2]) do
+    w3 =Matrex.add(w1,w2)
+    rest = sumNNs(t1,t2)
+    [w3|rest]
+
+  end
+  def slice_entries(1,input,target,weights,lr)do
+    input1 = Matrex.row(input,1)
+    target1 = Matrex.row(target,1)
+    task =Task.async(fn -> NN.runNet(input1,weights,target1,lr)end)
+    [task]
+  end
+  def slice_entries(n,input,target,weights,lr)do
+    input1 = Matrex.row(input,1)
+    target1 = Matrex.row(target,1)
+    task =Task.async(fn -> NN.runNet(input1,weights,target1,lr)end)
+    {il,ic}=Matrex.size(input)
+    {tl,tc}=Matrex.size(target)
+    restinput = Matrex.submatrix(input,2..il,1..ic)
+    resttarget = Matrex.submatrix(target,2..tl,1..tc)
+    slices = slice_entries(n-1,restinput,resttarget,weights,lr)
+    [task|slices]
+  end
 end
 
 
@@ -208,12 +248,12 @@ alpha = 0.005
 #                          [ 0.370439  ]])#NN.newDenseLayer(hiddenSize,outputSize,:relu)
 
 
-w0_ = Matrex.load("w01.csv")
-w1_ = Matrex.load("w12.csv")
+#w0_ = Matrex.load("w01.csv")
+#w1_ = Matrex.load("w12.csv")
 #nn = [w0_,w1_]
 
 inputSize = 784 #pixels per image
-hiddenSize = 2880#1440#720#360#40#360#180#40
+hiddenSize = 180#40#720#360#40#360#180#40
 outputSize = 10
 alpha = 0.005
 nn = [NN.newDenseLayer(inputSize,hiddenSize,:relu),
@@ -243,7 +283,7 @@ images = Matrex.load("imgMNIST.csv")
 labels = Matrex.load("tarMNIST.csv")
 
 input1 = images[1]
-
+target1 = labels[1]
 #r = NN.dotPSize(5,input1,wh)
 
 #IO.inspect input1
@@ -258,13 +298,53 @@ input1 = images[1]
 #time1 = Time.utc_now()
 #{newNet,errorFinal,correct} = NN.trainNN(1000,images,nn,labels,alpha)
 
-#{newNet,errorFinal,correct} = NN.loop(100,1000,images,nn,labels,alpha)
+#time1 = Time.utc_now()
+defmodule Bench do
+  def execNN(1,input1,nn,target1,alpha) do
+    #task1 = Task.async(fn -> NN.runNet(input1,nn,target1,alpha) end)
+    #task2 = Task.async(fn -> NN.runNet(input1,nn,target1,alpha) end)
+    #task3 = Task.async(fn -> NN.runNet(input1,nn,target1,alpha) end)
+    NN.runNet(input1,nn,target1,alpha)
+    #Task.await(task1)
+    #Task.await(task2)
+    #Task.await(task3)
+  end
+  def execNN(n,input1,nn,target1,alpha) do
+    NN.runNet(input1,nn,target1,alpha)
+    execNN(n-1,input1,nn,target1,alpha)
+  end
+  def execNNp(1,l,input1,nn,target1,alpha) do
+    task = Task.async(fn -> NN.runNet(input1,nn,target1,alpha) end)
+    Enum.map([task|l],&Task.await/1)
+    #Task.await(task1)
+    #Task.await(task2)
+    #Task.await(task3)
+  end
+  def execNNp(n,l,input1,nn,target1,alpha) do
+    task = Task.async(fn -> NN.runNet(input1,nn,target1,alpha) end)
+    execNNp(n-1,[task|l],input1,nn,target1,alpha)
+  end
+end
 
-{time,{newNet,errorFinal,correct} } = :timer.tc(&NN.loop/6,[1,1000,images,nn,labels,alpha])
+#{time,r} = :timer.tc(&Bench.execNNp/6,[100,[],input1,nn,target1,alpha])
+#{time,r} = :timer.tc(&Bench.execNN/5,[100,input1,nn,target1,alpha])
+#IO.puts ("time: #{(time)/(1_000_000)}")
 
+
+
+#{time,{newNet,errorFinal,correct} } = :timer.tc(&NN.loop/6,[1,1000,images,nn,labels,alpha])
+
+
+#{time,r}=:timer.tc(&NN.run_batch/5,[1000,images,nn,labels,alpha])
+
+{time,{newnet,error,correct}}=:timer.tc(&NN.trainNN/5,[1000,images,nn,labels,alpha])
 IO.puts ("time: #{time/(1_000_000)}")
-
 #timef = Time.diff(time2,time1)
+
+
+
+
+
 
 #IO.inspect timef
 #IO.puts("error")
